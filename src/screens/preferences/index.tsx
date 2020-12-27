457183c42e8904e1f31f9be0321394f5b487d7e5
bana118 <bana.titech@gmail.com>
2020-12-27 12:01:49 +0000

    add text input for preferences

--- src/screens/preferences/index.tsx
+++ src/screens/preferences/index.tsx
@@ -1,19 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React, { ReactElement, useState } from "react";
-import { StyleSheet, View, Text, Switch } from "react-native";
+import { StyleSheet, View, Text, Switch, TextInput } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../../App";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function PreferencesScreen({
   navigation,
 }: {
   navigation: PreferencesScreenNavigationProp;
 }): ReactElement {
+  const [endpoint, setEndpoint] = useState("127.0.0.1");
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
     <View style={styles.container}>
       <Text>Setting Screen</Text>
+      <TextInput onChangeText={(input) => setEndpoint(input)} />
       <Switch
         trackColor={{ false: "#767577", true: "#81b0ff" }}
         thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
