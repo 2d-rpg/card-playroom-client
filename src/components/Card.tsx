736dcba5aa66ea59cbd828537909ec35895e03df
bana118 <bana.titech@gmail.com>
2021-01-05 13:20:12 +0000

    contain image in component and set image height and width reference to screen height

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,26 @@
 import React, { ReactElement } from "react";
 import { Image } from "react-native";
+import { Dimensions } from "react-native";
 
 export default function Card(props: {
   facePath: string;
   backPath: string;
   endpoint: string;
 }): ReactElement {
-  // TODO サイズ調整
+  // TODO ズームイン，ズームアウト時の拡大縮小に対応
+  // TODO 画像読み込み失敗時の対応
+  const windowHeight = Dimensions.get("window").height;
+  const height = windowHeight * 0.2;
+  const width = (height * 2) / 3;
+  console.log(windowHeight);
   return (
     <Image
       style={{
-        width: 102,
-        height: 150,
+        margin: 10,
+        width: width,
+        height: height,
       }}
+      resizeMode={"contain"}
       source={{
         uri: `http://${props.endpoint}${props.facePath}`,
       }}
